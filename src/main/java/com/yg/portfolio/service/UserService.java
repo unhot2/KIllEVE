367d4613773b814f34814053a3ce56fcfda9aae2
최윤규 <unhot2@nate.com>
2021-11-08 23:05:05 +0900

    회원가입 연결

--- src/main/java/com/yg/portfolio/service/UserService.java
+++ src/main/java/com/yg/portfolio/service/UserService.java
@@ -24,8 +24,9 @@ public class UserService implements UserDetailsService{
 	// 파라매터 user네임은 loginForm에서 input에 name이 username이어야 인식이 됨
 	// 만약 바꾸려면 SecurityConfig에서 .usernameParameter("username2")와 같이 추가해주어야 함
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User userEntity = userRepository.findByUsername(username);
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+		System.out.println("userId 값 : "+userId);
+		User userEntity = userRepository.findByUsername(userId);
 		if (userEntity != null) {
 			// 시큐리티 session = Authentication = UserDetails
 			// UserDetails 가 리턴되면 Authentication 내부에 들어가고 그 Authentication은 시큐리티 session내부에 들어감
