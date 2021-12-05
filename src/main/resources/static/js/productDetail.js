ab3e8beccc0c943d37e9c8450aa5f4c8d4b0cfb4
최윤규 <unhot2@nate.com>
2021-12-05 21:25:39 +0900

    스프링부트 레이아웃 변경으로 인한 선언 변경 layout:decorator="layout/layout" -> layout:decorate="~{layout/layout}

--- src/main/resources/static/js/productDetail.js
+++ src/main/resources/static/js/productDetail.js
@@ -15,14 +15,68 @@ $(function() {
 		$('#select-size').val('*').prop("selected", true);
 	});
 	
+	$('#test').click(function(){
+		alert($('#salePriceText').text())
+	});
+	
+	// 색상,사이즈 선택 시 최종상품,가격 추가
 	$('#select-size').change(function(){
+		var productName = $('#productName').text();
+		var color = $('#select-color').val();
+		var size = $('#select-size').val();
+		var salePrice = $('#salePrice').text().replace('￦','').replace('원','').replace(',','');
 		if($('#select-color').val() != '*' && $('#select-size').val() != '*'){
-			var html = '<div>둘다 들어감</div>';
-			$('.totalProduct').append(html)	
+			var html = '<table class="total-table">\
+							<tr>\
+								<td class="totalTd1"><b>'+productName+'</b><br>-'+color+'/'+size+'</td>\
+								<td class="totalTd2">\
+									<span class="quantityArea">\
+										<input type="number" id="quantity" name="" class="quantity" value="1">\
+										<a href="#none" class="upBtn quantityBtn">\
+											<img src="//img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif" alt="수량증가">\
+										</a>\
+										<a href="#none" class="downBtn quantityBtn">\
+												<img src="//img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif" id="option_box1_down" class="option_box_down" alt="수량감소">\
+										</a>\
+									</span>\
+									<a href="#none" class="delete">\
+										<img src="//img.echosting.cafe24.com/design/skin/default/product/btn_price_delete.gif" alt="삭제" id="option_box1_del" class="option_box_del">\
+									</a>\
+								</td>\
+								<td class="totalTd3"><b id="salePriceText">￦'+priceToString(salePrice)+'원</b></td>\
+							</tr>\
+							</table>';
+			$('.totalProduct').append(html)
+			$('#totalPrice').text(priceToString(salePrice));
+			$('#quantityText').text($('#quantity').val());	
 			$('#select-color').val('*').prop("selected", true);
 			$('#select-size').val('*').prop("selected", true);		
 		}
 	});
 	
+	$(document).on('click','.upBtn', function(){
+		var quantity = parseInt($('#quantity').val());
+		$('#quantity').val(quantity+1)
+		var salePrice = $('#salePrice').text().replace('￦','').replace('원','').replace(',','');
+		$('#salePriceText').text('￦'+priceToString(salePrice*(quantity+1))+'원');
+	});
+	
+	$(document).on('click','.downBtn', function(){
+		var quantity = parseInt($('#quantity').val());
+		if(quantity > 1){
+			$('#quantity').val(quantity-1)	
+			var salePrice = $('#salePrice').text().replace('￦','').replace('원','').replace(',','');
+			$('#salePriceText').text('￦'+priceToString(salePrice*(quantity-1))+'원');
+		}
+		else {
+			alert('최소 주문수량은 1개 입니다.');
+		}
+	});
+	
+	$('#salePriceText').on('change',function(){
+		alert('바귐')
+/*		var d = $('#salePriceText').text()
+		$('#totalPrice').text($('#salePriceText').text())
+*/	})
 });
 
